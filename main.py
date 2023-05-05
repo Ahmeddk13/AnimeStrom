4d28624aa18e2e2759298819a8136519e0219435
AHmed Dikha <126340494+Ahmeddk13@users.noreply.github.com>
2023-05-05 02:56:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from flask import Flask , request
-from WAnime import *
-app = Flask(__name__)
+import requests as req
 
-Domain ="https://witanime.com/anime-type/"
-Classer = "anime-list-content"
-
-#Home list page 
-
-@app.route('/list')
-def Home():
-    posi_list = request.args.get('list')
-    return GetList_Anime(posi_list=posi_list)
-
-#MOVIES LIST PAGE
-
-@app.route('/Movie/')
-def Movie():
-    return GetList_Anime(Domain+'movie/',Classer)
-
-#ANIME LIST PAGE
-
-@app.route('/Anime/')
-def Anime():
-    return GetList_Anime(Domain+'anime/',Classer)
-
-app.run(debug=True,host='0.0.0.0', port=81)
+while True:
+    r = req.get("https://admin-panel-kitabi.ahmeddk13.repl.co")
