299460ccfad262ef4d876ffd7cabaf9ca39534f6
AHmed Dikha <126340494+Ahmeddk13@users.noreply.github.com>
2023-05-05 20:37:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 while True:
     time.sleep(60)
     try :
-        r = req.get("https://admin-panel-kitabi.ahmeddk13.repl.co")
+        r = req.head("https://admin-panel-kitabi.ahmeddk13.repl.co")
         rr = req.get("https://kotobati.ahmeddk13.repl.co/")
     except Exception :
         print("Error")
