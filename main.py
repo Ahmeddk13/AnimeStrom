2619680ea3dc06e5a3f5c77dd83b20182a708859
AHmed Dikha <126340494+Ahmeddk13@users.noreply.github.com>
2023-05-05 12:18:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests as req
 import time
 while True:
-    time.sleep(1.5)
-    r = req.get("https://admin-panel-kitabi.ahmeddk13.repl.co")
-    rr = req.get("https://kotobati.ahmeddk13.repl.co/")
+    time.sleep(3)
+    try :
+        r = req.get("https://admin-panel-kitabi.ahmeddk13.repl.co")
+        rr = req.get("https://kotobati.ahmeddk13.repl.co/")
+    except Exception :
+        print("Error")
