b27773f22cdc5b6b9e942f324ef0ac3dbebb2b4c
AHmed Dikha <126340494+Ahmeddk13@users.noreply.github.com>
2023-05-05 12:49:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests as req
 import time
 while True:
-    time.sleep(3000)
+    time.sleep(60)
     try :
         r = req.get("https://admin-panel-kitabi.ahmeddk13.repl.co")
         rr = req.get("https://kotobati.ahmeddk13.repl.co/")
