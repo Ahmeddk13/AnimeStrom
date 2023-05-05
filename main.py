bbb8e3523199ae36953561df97965e0c744619d0
AHmed Dikha <126340494+Ahmeddk13@users.noreply.github.com>
2023-05-05 03:01:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests as req
-
+import time
 while True:
+    time.sleep(1.5)
     r = req.get("https://admin-panel-kitabi.ahmeddk13.repl.co")
+    rr = req.get("https://kotobati.ahmeddk13.repl.co/")
